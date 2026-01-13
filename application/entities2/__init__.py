2242c8affe435b34082df0ba2640849489aa9ce5
Knebler <builderguy665@gmail.com>
2026-01-13 15:43:15 +0800

    Updating testimonials to become dynamic

--- application/entities2/__init__.py
+++ application/entities2/__init__.py
@@ -10,6 +10,7 @@ from .subscription import SubscriptionModel
 from .subscription_plans import SubscriptionPlanModel
 from .user import UserModel
 from .venue import VenueModel
+from .testimonial import TestimonialModel
 
 
 
